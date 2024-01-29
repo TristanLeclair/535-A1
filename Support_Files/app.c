dfd080eee052dd8a2031c527bcc26269c26e987a
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-01-29 11:50:03 -0500

    Update to new support files

--- Support_Files/app.c
+++ Support_Files/app.c
@@ -13,9 +13,9 @@ int main() {
   char *names[10];
   rv = zcs_query("type", "speaker", names, 10);
   if (rv > 0) {
-    int num = 5;
-    zcs_attribute_t attrs[num];
-    rv = zcs_get_attribs(names[0], attrs, &num);
+    zcs_attribute_t attrs[5];
+    int anum = 5;
+    rv = zcs_get_attribs(names[0], attrs, &anum);
     if ((strcmp(attrs[0].attr_name, "location") == 0) &&
         (strcmp(attrs[0].value, "kitchen") == 0)) {
       rv = zcs_listen_ad(names[0], hello);
