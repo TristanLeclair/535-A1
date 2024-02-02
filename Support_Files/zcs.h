fecefd0cd015ae42e63236f2a289f5afa8b27b98
Ben Halpern <beneddiehalpern@gmail.com>
2024-02-02 15:59:29 -0500

    move helper functions back into zcs

--- Support_Files/zcs.h
+++ Support_Files/zcs.h
@@ -9,31 +9,6 @@ typedef struct {
     char *value;
 } zcs_attribute_t;
 
-typedef struct _zcs_node_t {
-  struct sockaddr_in address;
-  char *name;
-  int status;                         // 0 for down, 1 for up
-  time_t hearbeat_time;
-  struct _zcs_node_t *next;
-  zcs_attribute_t *attriutes[];
-} zcs_node_t;
-
-typedef struct _node_list_t {
-  zcs_node_t *head;
-  zcs_node_t *tail;
-} node_list_t;
-
-typedef struct ad_node {
-  char *name;
-  zcs_cb_f cback;
-  struct ad_node *next;
-} ad_node_t;
-
-typedef struct ad_list {
-  ad_node_t *head;
-  ad_node_t *tail;
-} ad_list_t;
-
 typedef void (*zcs_cb_f)(char *, char *);
 
 int zcs_init(int type);
