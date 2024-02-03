56a65a52490ca1c9765078073b3330a74a97795a
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-02-03 16:31:26 -0500

    fix: formatting and play with ports

--- src/send_test.c
+++ src/send_test.c
@@ -1,30 +1,31 @@
 #include "../include/multicast.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 char buffer[100];
 
 int main(int argc, char *argv[]) {
 
-    if (argc < 3) {
-	    printf("Usage: send_test send-to-port msg \n");
-	    exit(0);
-    }
-    
-    int sport = atoi(argv[1]);
-    printf("Send-to port: %d\n", sport);
-    srand(0);
-    int myport = sport + random() % sport;
-    printf("Receiving port: %d\n", myport);
-    char *msg = argv[2];
+  if (argc < 3) {
+    printf("Usage: send_test send-to-port msg \n");
+    exit(0);
+  }
 
-    mcast_t *m = multicast_init("224.1.1.1", sport, myport);
+  int sport = atoi(argv[1]);
+  printf("Send-to port: %d\n", sport);
+  srand(0);
+  // int myport = sport + random() % sport;
+  int myport = 5000;
+  printf("Receiving port: %d\n", myport);
+  char *msg = argv[2];
 
-    while (1) {
-        sleep(1);
-        printf("Sending.. \n");
-        multicast_send(m, msg, strlen(msg));
-    }
+  mcast_t *m = multicast_init("225.1.1.1", sport, myport);
+
+  while (1) {
+    sleep(1);
+    printf("Sending.. \n");
+    multicast_send(m, msg, strlen(msg));
+  }
 }
