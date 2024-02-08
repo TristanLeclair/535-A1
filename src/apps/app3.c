c1374a43df3377fa5be432ccd043dd6763ac6920
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-02-08 11:38:58 -0500

    fix: differentiate apps and services

--- src/apps/app3.c
+++ src/apps/app3.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 void hello(char *s, char *r) {
-  printf("Ad received in app: %s, with value: %s\n", s, r);
+  printf("Ad received in app3: %s, with value: %s\n", s, r);
   zcs_log();
 }
 
@@ -24,7 +24,7 @@ int main() {
       int anum = 5;
       rv = zcs_get_attribs(names[0], attrs, &anum);
       if ((strcmp(attrs[1].attr_name, "location") == 0) &&
-          (strcmp(attrs[1].value, "kitchen") == 0)) {
+          (strcmp(attrs[1].value, "basement") == 0)) {
         rv = zcs_listen_ad(names[0], hello);
       }
     }
--- src/services/service3.c
+++ src/services/service3.c
@@ -6,9 +6,9 @@ int main() {
     rv = zcs_init(ZCS_SERVICE_TYPE);
     zcs_attribute_t attribs[] = {
 	    { .attr_name = "type", .value = "speaker"},
-	    { .attr_name = "location", .value = "basement"},
+	    { .attr_name = "location", .value = "livingroom"},
 	    { .attr_name = "make", .value = "yamaha"} };
-    rv = zcs_start("speaker-Y", attribs, sizeof(attribs)/sizeof(zcs_attribute_t));
+    rv = zcs_start("speaker-Z", attribs, sizeof(attribs)/sizeof(zcs_attribute_t));
     for (int i = 0; i < 1000; i++) {
         rv = zcs_post_ad("mute", "on");
         sleep(10);
