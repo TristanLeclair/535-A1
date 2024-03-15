825a24865bb0f50f08035ac2b046d8f44166501e
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-03-15 16:08:44 -0400

    start relay

---
+++ src/relay/relay.c
@@ -0,0 +1,9 @@
+#include "multicast/multicast.h"
+#include "networking/networking.h"
+
+int main() {
+  mcast_t m_rec_a = multicast_init(LAN_IP_APP_A, )
+  mcast_t m_rec_b;
+  mcast_t m_send_a;
+  mcast_t m_send_b;
+}
